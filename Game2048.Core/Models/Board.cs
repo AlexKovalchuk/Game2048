985e1fcee544b2df78a12b21cbb5eb8bd0ac622f
Oleksandr Kovalchuk <oleksandr.kovalchuk@avenga.com>
2025-10-10 16:21:00 +0200

    Step 1: create IRandom and base Board, create playground for the testing

---
+++ Game2048.Core/Models/Board.cs
@@ -0,0 +1,31 @@
+﻿using Game2048.Core.Abstractions;
+using Game2048.Core.Enums;
+
+namespace Game2048.Core.Models;
+
+public class Board
+{
+    List<Tile> Tiles { get; }
+    public int Size { get; private set; }
+    public int Score { get; private set; }
+    public GameState State { get; private set; }
+    IRandom Random { get; set; }
+
+    public Board(int size, IRandom random)
+    {
+        Size = size >=2 ? size : 4;
+        Score = 0;
+        State = GameState.InProgress;
+        Tiles = new List<Tile>();
+        Random = random;
+        
+        if (size < 2) throw new ArgumentException("Board size must be >= 2");
+    }
+
+    public void Clear()
+    {
+        Score = 0;
+        State = GameState.InProgress;
+        Tiles?.Clear();
+    }
+}
---
+++ Game2048.Playground/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Game2048.Core.Models;
+using Game2048.Core.Infrastructure;
+
+Console.WriteLine("Hello, World! This is 2048 playground");
+var board = new Board(4, new SystemRandom());
+board.Clear();
